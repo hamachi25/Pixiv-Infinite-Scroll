2ec2738b4e743ab9fa856844d9b456f092891297
hamachi25 <107383950+hamachi25@users.noreply.github.com>
2025-01-18 17:27:05 +0900

    v2.0.1

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	manifest: {
 		name: "Pixiv Infinite Scroll",
 		description: "__MSG_extension_extDescription__",
-		version: "2.0.0",
+		version: "2.0.1",
 		default_locale: "en",
 		permissions: ["storage"],
 	},
