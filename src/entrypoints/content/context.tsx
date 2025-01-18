b239936b31accd070f73678720732a17728e099a
hamachi25 <107383950+hamachi25@users.noreply.github.com>
2025-01-18 17:18:05 +0900

    既存要素のリンクも新規タブで開く

--- src/entrypoints/content/context.tsx
+++ src/entrypoints/content/context.tsx
@@ -1,6 +1,11 @@
 import { createContext } from "react";
 import type { Settings } from "@/types/storage";
 import { settingsItem } from "@/utils/storage";
+import { getElementSelectorByUrl } from "./utils/getElementSelectorByUrl";
+
+interface HTMLAnchorElementWithHandleClick extends HTMLAnchorElement {
+	_handleClick?: (e: MouseEvent) => void;
+}
 
 export const SettingContext = createContext<Settings | undefined>(undefined);
 export const CsrfContext = createContext<React.RefObject<string | undefined>>({
@@ -21,6 +26,35 @@ export const Context = ({ children }: { children: React.ReactNode }) => {
 		});
 		return () => unwatch();
 	}, []);
+
+	// リンクを新規タブで開く
+	useEffect(() => {
+		const anchor = getElementSelectorByUrl(location);
+		const firstPageElement = document.querySelector(anchor);
+		if (!firstPageElement) return;
+
+		const links = firstPageElement.querySelectorAll<HTMLAnchorElementWithHandleClick>("a");
+
+		if (settings?.openInNewTab) {
+			links.forEach((link) => {
+				const handleClick = (e: MouseEvent) => {
+					e.stopPropagation();
+				};
+				link.addEventListener("click", handleClick);
+				link.setAttribute("target", "_blank");
+
+				link._handleClick = handleClick;
+			});
+		} else {
+			links.forEach((link) => {
+				if (link._handleClick) {
+					link.removeEventListener("click", link._handleClick);
+					delete link._handleClick;
+				}
+				link.removeAttribute("target");
+			});
+		}
+	}, [settings]);
 
 	return (
 		<SettingContext value={settings}>
