9c92b4fbba9fbe09ae2cdcbd73771078d4ca4514
hamachi25 <107383950+hamachi25@users.noreply.github.com>
2025-01-20 00:31:38 +0900

    整理

--- src/entrypoints/content/utils/storage.ts
+++
@@ -1,8 +0,0 @@
-import type { Settings } from "@/types/storage";
-
-export const settingsItem = storage.defineItem<Settings>("local:settings", {
-	fallback: {
-		openInNewTab: false,
-	},
-	version: 1,
-});
