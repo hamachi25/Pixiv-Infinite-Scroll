39ead48c7718d94cd007e3822fad65001984dc7c
hamachi25 <107383950+hamachi25@users.noreply.github.com>
2025-02-22 10:11:15 +0900

    chore: 多言語対応

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 	modules: ["@wxt-dev/module-react", "@wxt-dev/i18n/module", "@wxt-dev/auto-icons"],
 	manifest: {
 		name: "Pixiv Infinite Scroll",
-		description: "__MSG_extension_extDescription__",
+		description: "__MSG_description__",
 		default_locale: "en",
 		permissions: ["storage"],
 	},
