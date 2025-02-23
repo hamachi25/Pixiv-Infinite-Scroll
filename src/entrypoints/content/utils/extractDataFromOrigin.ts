635306ec7ad5c1a75c4612a61afa684fc9d33399
hamachi <107383950+hamachi25@users.noreply.github.com>
2025-02-23 12:08:51 +0900

    fix: ブックマークやフォローボタンが動作しないバグを修正 (#18)

--- src/entrypoints/content/utils/extractDataFromOrigin.ts
+++ src/entrypoints/content/utils/extractDataFromOrigin.ts
@@ -1,9 +1,9 @@
 export const extractCsrfToken = (html: string | null): string | undefined => {
-	const token = html?.match(/"token":"([a-f0-9]+)"/)?.[1];
+	const token = html?.match(/"token\\":\\"([a-f0-9]+)\\"/)?.[1];
 	return token;
 };
 
 export const extractIsSensitive = (html: string | null): boolean => {
-	const isNotSensitive = html?.match(/"isSensitiveViewable":(true|false)/)?.[1];
+	const isNotSensitive = html?.match(/"isSensitiveViewable\\":(true|false)/)?.[1];
 	return isNotSensitive === "false";
 };
