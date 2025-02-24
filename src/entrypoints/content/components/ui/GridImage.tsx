637b1673bdc0b4fbe09bee3722b11c4a41af6b1b
hamachi25 <107383950+hamachi25@users.noreply.github.com>
2025-02-24 19:19:54 +0900

    chore: svgをコンポーネント化

--- src/entrypoints/content/components/ui/GridImage.tsx
+++ src/entrypoints/content/components/ui/GridImage.tsx
@@ -3,6 +3,7 @@ import { SettingContext, SensitiveContext } from "@content/context";
 import { IllustCount } from "./IllustCount";
 import { UgoiraOverlay } from "../icons/illust/UgoiraOverlay";
 import { SensitiveOverlay } from "../icons/illust/SensitiveOverlay";
+import { MutedImage } from "../icons/MutedImage";
 
 interface Props {
 	illust: Work;
@@ -22,9 +23,7 @@ export const GridImage = ({ illust, type }: Props) => {
 			{illust.isMuted && type !== "bookmark" ? (
 				<div className="flex h-[184px] w-[184px] items-center justify-center rounded-[8px] bg-[var(--charcoal-background2)]">
 					<div className="-mb-[1px] flex h-[122px] w-[122px] select-none flex-col items-center justify-center gap-[4px] text-[var(--charcoal-text4)]">
-						<svg className="h-[48px] w-[48px] fill-current" viewBox="0 0 24 24">
-							<path d="M5.26763775,4 L9.38623853,11.4134814 L5,14.3684211 L5,18 L13.0454155,18 L14.1565266,20 L5,20 C3.8954305,20 3,19.1045695 3,18 L3,6 C3,4.8954305 3.8954305,4 5,4 L5.26763775,4 Z M9.84347336,4 L19,4 C20.1045695,4 21,4.8954305 21,6 L21,18 C21,19.1045695 20.1045695,20 19,20 L18.7323623,20 L17.6212511,18 L19,18 L19,13 L16,15 L15.9278695,14.951913 L9.84347336,4 Z M16,7 C14.8954305,7 14,7.8954305 14,9 C14,10.1045695 14.8954305,11 16,11 C17.1045695,11 18,10.1045695 18,9 C18,7.8954305 17.1045695,7 16,7 Z M7.38851434,1.64019979 L18.3598002,21.3885143 L16.6114857,22.3598002 L5.64019979,2.61148566 L7.38851434,1.64019979 Z"></path>
-						</svg>
+						<MutedImage className="h-[48px] w-[48px]" />
 					</div>
 				</div>
 			) : (
