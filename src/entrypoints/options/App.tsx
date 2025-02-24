167a8ba497604fc9e455654e327e9756fbce3880
hamachi25 <107383950+hamachi25@users.noreply.github.com>
2025-02-25 00:20:02 +0900

    style: margin調整

--- src/entrypoints/options/App.tsx
+++ src/entrypoints/options/App.tsx
@@ -30,18 +30,18 @@ const muteSectionContent = {
 const App = () => {
 	return (
 		<div
-			className="mx-auto flex h-screen max-w-[65rem] flex-col p-6"
+			className="mx-auto flex h-screen max-w-[65rem] flex-col px-6"
 			dir={isRTL ? "rtl" : "ltr"}
 		>
-			<div className="flex items-center justify-between">
+			<div className="mb-3 mt-5 flex items-center justify-between">
 				<h1 className="text-xl font-bold">{i18n.t("options.title")}</h1>
 				<div className="flex gap-4">
 					<ImportButton />
 					<ExportButton />
 				</div>
 			</div>
-			<div className="divider" />
-			<div className="flex w-full flex-1 gap-4 overflow-hidden">
+			<div className="divider mt-0" />
+			<div className="mb-6 flex w-full flex-1 gap-4 overflow-hidden">
 				<TagMuteSection content={muteSectionContent.tags} />
 				<UserMuteSection content={muteSectionContent.users} />
 			</div>
